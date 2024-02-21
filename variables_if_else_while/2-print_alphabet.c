12c4696dff23ae5e24a007f7e83ce427064984d2
Gadiel02 <gadielrivera02@gmail.com>
2024-02-21 10:20:09 -0400

    file 1 int 0

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#iclude <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always o (success)
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar("\n");
+	return (0);
+}
