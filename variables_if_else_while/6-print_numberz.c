cd9f257eca19822467d87206a2a5ff9f54326de9
Gadiel02 <gadielrivera02@gmail.com>
2024-02-21 14:10:08 -0400

    add file 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always o (success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		if (n <= 9)
+			n++;
+	}
+	putchar ('\n');
+	return (0);
+}
