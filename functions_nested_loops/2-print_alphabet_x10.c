11a7cd2a73538256080cbf88545f1880e527a86a
Gadiel02 <gadielrivera02@gmail.com>
2024-02-23 08:27:00 -0400

    filr 2 print alphabet 10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int num, letter;
+
+	for (letter = 0; letter <= 9; letter++)
+	{
+		for (num = 97; num < 122; num++)
+		{
+			_putchar(num);
+		}
+		_putchar('\n');
+	}
+}
