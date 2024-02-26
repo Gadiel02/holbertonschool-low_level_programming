f47f7dc7daa15360d9affea122dd40100d4d8db0
Gadiel02 <gadielrivera02@gmail.com>
2024-02-25 21:46:52 -0400

    file 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,13 +1,14 @@
 #include"main.h"
 /**
  *jack_bauer- Entry point
- *@h,m: an argument
+ *@ h, m: an argument
  *Return: Always 0 (Success)
  */
 
 void jack_bauer(void)
 {
 	int h, m;
+
 	for (h = 0; h <= 23; h++)
 	{
 		for (m = 0; m <= 59; m++)
