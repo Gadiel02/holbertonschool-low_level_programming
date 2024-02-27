b290bd1f70ac393a7d272e481e2359f4749b32bd
Gadiel02 <gadielrivera02@gmail.com>
2024-02-27 10:18:06 -0400

    file 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ *more_numbers - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int k, f;
+
+		for (k = 0; k <= 10; k++)
+		{
+			for (f = 0; f <= 14; f++)
+			{
+				if (f > 9)
+				{
+					_putchar((f / 10) + '0');
+				}
+				_putchar((f % 10) + '0');
+			}
+			_putchar('\n');
+		}
+}
