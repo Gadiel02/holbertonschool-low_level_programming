ada9b1d3f564df24f93bd2582fbbe5399a43401c
Gadiel02 <gadielrivera02@gmail.com>
2024-02-27 09:02:11 -0400

    filr 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ *print_most_numbers - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+		for (num = 0; num <= 9; num++)
+		{
+			if (num != 2 && 4)
+			{
+				_putchar(num);
+			}
+		}
+}
