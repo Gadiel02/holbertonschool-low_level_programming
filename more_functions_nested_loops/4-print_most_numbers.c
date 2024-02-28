d5607b3d1f0e3bd594adfc7f627ec67a01b71abe
Gadiel02 <gadielrivera02@gmail.com>
2024-02-27 20:22:10 -0400

    file 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,17 @@ void print_most_numbers(void)
 {
 	int num;
 
-		for (num = 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
 		{
-			if (num != 2 && 4)
-			{
-				_putchar(num);
-			}
+			_putchar(num);
+
 		}
+		else
+		{
+			_putchar((num) + '0');
+		}
+	}
+	_putchar('\n');
 }
