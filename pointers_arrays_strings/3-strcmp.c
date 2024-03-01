507cf1b2ab7a3150e4350c48f8781cf754caedb8
Gadiel02 <gadielrivera02@gmail.com>
2024-03-01 11:41:34 -0400

    file 3 int 1

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -6,15 +6,15 @@
 int _strcmp(char *s1, char *s2)
 {
 	int z;
-
-	z = s1 -  s2;
+	int index = 0;
+	
+	z = s1[index] - s2[index];
 	while (z == 0)
 	{
-		s1++;
-		s2++;
-
-		if (*s1 == '\0')
+		if (s1[index] == '\0')
 			break;
+		index++;
+		z = s1[index] - s2[index];
 	}
 	return (z);
 }
