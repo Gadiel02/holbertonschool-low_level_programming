536aa7537fa825151becb4e9f4c2af792e17f81e
Gadiel02 <gadielrivera02@gmail.com>
2024-03-01 21:21:31 -0400

    file 0 int 1

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,17 @@
+#include "main.h"
+/**
+ *string_toupper - Entry point
+ *@lin: an argument
+ *Return: Always 0 (Success)
+ */
 char *string_toupper(char *lin)
 {
 	int i;
-	
-	for (i = 0; lin[i] != '\0';i++)
+
+	for (i = 0; lin[i] != '\0'; i++)
 	{
 		if (lin[i] >= 'a' && lin[i] <= 'z')
-			lin[i] = lin[i] -32;
+			lin[i] = lin[i] - 32;
 	}
 	return (lin);
 }
