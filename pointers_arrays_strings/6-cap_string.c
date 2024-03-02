43e78149bc77da5077b0bd7b9fe6a780e3af7229
Gadiel02 <gadielrivera02@gmail.com>
2024-03-01 22:42:00 -0400

    file 6 int 0

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *cap_string - Entry point
+ *@upe: an argument
+ *Return: Always 0 (Success)
+ */
+char *cap_string(char *upe)
+{
+	int i;
+
+	for (i = 0; upe[i] != '\0'; i++)
+	{
+		if (i == 0 || upe[i - 1] == ' ' || upe[i - 1] == '.' || upe[i - 1] == '\n')
+		{0
+			if (upe[i] >= 'a' && upe[i] <= 'z')
+				upe[i] -= 32;
+		}
+	}
+	return (upe);
+}
