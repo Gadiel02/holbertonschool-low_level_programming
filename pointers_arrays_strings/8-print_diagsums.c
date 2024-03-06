ef95933d074d2fc66427cc8cf58381cb719175bc
Gadiel02 <gadielrivera02@gmail.com>
2024-03-05 20:21:19 -0400

    file 8 int 1

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *print_diagsum - Entry point
+ *print_diagsums - Entry point
  *@a: an argument
+ *@size: an argument
  *Return: Always 0 (Success)
  */
 void print_diagsums(int *a, int size)
@@ -16,12 +17,12 @@ void print_diagsums(int *a, int size)
 		suma0 += a[num];
 		a += size;
 	}
+
 	a -= size;
-	
 	for (num = 0; num < size; num++)
 	{
 		suma1 += a[num];
-		a-= size;
+		a -= size;
 	}
 	printf("%d, %d\n", suma0, suma1);
 }
