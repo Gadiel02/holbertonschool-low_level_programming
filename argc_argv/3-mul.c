823c0c3599bd554f3de4031b3842bcd56617ea57
Gadiel02 <gadielrivera02@gmail.com>
2024-03-07 14:08:01 -0400

    file 3 int 0

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, int argva[])
+{
+	int n1, n2
+		
+		if(argc == 3)
+		{
+			n1 = argv[1]
+			n2 = argv[2]
+			print("%d\n", n1 * n2);
+		}
+		else
+		{
+			printf("error\n");
+			return (1);
+		}
+		return (0):
+
+}
