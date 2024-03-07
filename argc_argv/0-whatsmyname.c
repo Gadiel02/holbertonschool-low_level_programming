775267d95a3f43e72ad15eb0c7dd0eb53eb8c8eb
Gadiel02 <gadielrivera02@gmail.com>
2024-03-07 12:04:46 -0400

    file 0 int 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *@argc: an argument
+ *@argv: an argument
+ *Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
