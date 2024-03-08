f12a13d15630edffdfa298b6e1a09bd14e0fa38f
Gadiel02 <gadielrivera02@gmail.com>
2024-03-07 21:18:38 -0400

    file 3 int 0

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
-
-int main(int argc, int argva[])
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *@argc: an argument
+ *@argv: an argument
+ *Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
-	int n1, n2
-		
-		if(argc == 3)
-		{
-			n1 = argv[1]
-			n2 = argv[2]
-			print("%d\n", n1 * n2);
-		}
-		else
-		{
-			printf("error\n");
-			return (1);
-		}
-		return (0):
+	int n1, n2;
+
+	if (argc == 3)
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+	return (0);
 
 }
