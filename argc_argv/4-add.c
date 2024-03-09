4d723798858a85d7a54db41580d7cac1ee6d47f3
Gadiel02 <gadielrivera02@gmail.com>
2024-03-08 22:57:49 -0400

    file 4 int 0

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - Entry point
+ *@argc: an argument
+ *@argv: an argument
+ *Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, digit, sum = 0;
+
+		for (i = 1; i < argc; i++)
+		{
+			for (digit = 0; argv[i][digit]; digit++)
+			{
+				if (argv[i][digit] < '0' || argv[i][digit] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
+	printf("%d\n", sum);
+
+	return (0);
+}
