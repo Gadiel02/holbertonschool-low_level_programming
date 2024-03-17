836e1bc7dd118f9db14a497083cfb74a36e0cf0a
Gadiel02 <gadielrivera02@gmail.com>
2024-03-16 22:42:45 -0400

    betty en file 1

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,16 @@
 #ifndef _VARIADIC_FUNCTION_H_
 #define _VARIADIC_FUCTION_H_
 #include <stdarg.h>
+/**
+ * struct funckey - a key for picking functions
+ * @f: function
+ * @spec: character specifier
+ */
+typedef struct funckey
+{
+	void (*f)(va_list);
+	char spec;
+} funckey;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
