ed8fe143b49c2bdcc20be772bc7dce91e000eb8f
Gadiel02 <gadielrivera02@gmail.com>
2024-04-02 08:30:46 -0400

    adding betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: elements in the list.
+ */
 size_t print_list(const list_t *h)
 {
 	size_t i;
--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,7 +1,11 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdlib.h>
-
+/**
+  * list_len - return the numbersof elements in a list
+  * @h: singly linked list
+  * Return: elements in a linked list
+  */
 size_t list_len(const list_t *h)
 {
 	int i;
