6ecfe4a13d86460c234240560c916faa3186d32c
Gadiel02 <gadielrivera02@gmail.com>
2024-04-03 13:28:43 -0400

    add betty

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stddef.h>
 #include <stdio.h>
+/**
+  * print_dlistint - doubly linked list
+  * @h: Head of the list
+  * Return: items in the linked list
+  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
